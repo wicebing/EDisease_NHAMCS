48b50facb9e4b074382bba0e3db7deae2bab0301
wicebing <32655081+wicebing@users.noreply.github.com>
2020-11-09 18:25:00 +0800

    Add files via upload

--- AIED_train_nhamcs_v031.py
+++ AIED_train_nhamcs_v031.py
@@ -29,7 +29,7 @@ try:
     task = sys.argv[1]
     print('*****task= ',task)
 except:
-    task = 'count_nhamcs_cls'
+    task = 'test_nhamcs_cls'
 
 try:
     pi = sys.argv[2]
@@ -41,7 +41,7 @@ batch_size = 1024
 device = 'cuda'
 parallel = False
 
-checkpoint_file = './checkpoint_emb/Lvp7_none'
+checkpoint_file = './checkpoint_emb/Revision_dim'
 
 if pi=='1':
     use_pi =True
@@ -742,6 +742,7 @@ def train_NHAMCS_cls_val(DS_model,
     debugnan = []
     LargeLossDebug =[]
     aucs = []
+    best_auc = 0
     
     for ep in range(epoch):   
         t0 = time.time()
@@ -817,7 +818,7 @@ def train_NHAMCS_cls_val(DS_model,
                     print('** error save checkpoint **')
                     pass
             iteration +=1
-        best_auc = 0
+
         if ep % 1 ==0:
             save_checkpoint(checkpoint_file=checkpoint_file,
                             checkpoint_path='EDisease_CLS.pth',
@@ -827,10 +828,14 @@ def train_NHAMCS_cls_val(DS_model,
                             checkpoint_path='CLS.pth',
                             model=cls_model,
                             parallel=parallel)
-            DS_model.eval() 
-            cls_model.eval()
             
-            _, auc = count_tsne_cls(net_F=DS_model,
+            val_model = AIED_bert.ewed_Model(config=config,
+                                 tokanizer=BERT_tokenizer,
+                                 device=device)
+            
+            val_DS_model = load_checkpoint(checkpoint_file,'EDisease_CLS.pth',val_model)
+            
+            _, auc = count_tsne_cls(net_F=val_DS_model,
                                baseBERT=baseBERT,
                                cls_model=cls_model,
                                target=dloader_val,
@@ -839,15 +844,14 @@ def train_NHAMCS_cls_val(DS_model,
                                ki=ep,
                                draw=False)
             
-
             aucs.append([ep,auc])
             
             pd_aucs = pd.DataFrame(aucs)
             pd_aucs.to_csv('./loss_record/aucs.csv', sep = ',')
             
+            cls_model.train()
             if auc > best_auc:
-                DS_model.train() 
-                cls_model.train()
+                # cls_model.train()
                 save_checkpoint(checkpoint_file=checkpoint_file,
                                 checkpoint_path='EDisease_CLS_BEST.pth',
                                 model=DS_model,
@@ -1169,6 +1173,210 @@ def train_NHAMCS_cls_dim(DS_model,
         pd_total_loss.to_csv('./loss_record/total_loss_finetune_CLS.csv', sep = ',')
     print(total_loss)  
 
+def train_NHAMCS_cls_dim_val(DS_model,
+                     cls_model,
+                     dim_model,
+                     baseBERT,
+                     dloader,
+                     dloader_val,
+                     checkpoint_file,
+                     noise_scale=0.05,
+                     mask_ratio=0.33,
+                     mix_ratio=1,
+                     lr=1e-4,
+                     epoch=100,
+                     log_interval=10,
+                     parallel=parallel,
+                     trainED=True,
+                     task=None,
+                     use_pi=False):
+    DS_model.to(device)
+    cls_model.to(device)
+    baseBERT.to(device)
+    dim_model.to(device)
+        
+    model_optimizer = optim.Adam(DS_model.parameters(), lr=lr)
+    model_optimizer_cls = optim.Adam(cls_model.parameters(), lr=lr)
+    model_optimizer_dim = optim.Adam(dim_model.parameters(), lr=lr)
+    if parallel:
+        DS_model = torch.nn.DataParallel(DS_model)
+        cls_model = torch.nn.DataParallel(cls_model)
+        baseBERT = torch.nn.DataParallel(baseBERT)
+        dim_model = torch.nn.DataParallel(dim_model)
+    else:
+        if device == 'cuda':
+            torch.cuda.set_device(0)
+            
+    criterion = nn.CrossEntropyLoss(ignore_index=-1).to(device)
+    #criterion = nn.MSELoss()
+    iteration = 0
+    total_loss = []
+    
+    debug = []
+    debugnan = []
+    LargeLossDebug =[]
+    aucs = []
+    best_auc = 0
+
+    for ep in range(epoch):   
+        t0 = time.time()
+        epoch_loss = 0
+        epoch_cases =0
+        
+        for batch_idx, sample in enumerate(dloader):
+            # if batch_idx > int(len(dloader)/5):
+            #     break
+            #print(iteration, sample['idx'])
+            model_optimizer.zero_grad()
+            model_optimizer_cls.zero_grad()
+            model_optimizer_dim.zero_grad()
+            
+            loss = 0
+
+            mode = 'D' if batch_idx%2==0 else 'G'
+                       
+            ptloss = True if batch_idx%699==3 else False
+            
+            output,EDisease, (s,input_emb,input_emb_org), (CLS_emb_emb,SEP_emb_emb),(c_emb,h_emb_mean,p_emb,yespi), nohx, expand_data = DS_model(baseBERT,sample,noise_scale=noise_scale,mask_ratio=mask_ratio,use_pi=False,)
+
+            aug2 = 2*random.random()
+            output2,EDisease2, (s,input_emb2,input_emb_org2), _,_, _, _ = DS_model(baseBERT,sample,noise_scale=aug2*noise_scale,mask_ratio=mask_ratio,use_pi=False)       
+            
+            bs = len(s)            
+
+            loss_dim = dim_model(output[:,:1],
+                             input_emb_org,
+                             CLS_emb_emb,
+                             nohx,
+                             mask_ratio=mask_ratio,
+                             mode=mode,
+                             ptloss=ptloss,
+                             DS_model=DS_model,
+                             mix_ratio=mix_ratio,
+                             EDisease2=output2[:,:1],
+                             shuffle=True,
+                             use_pi=use_pi,
+                             yespi=yespi,
+                             ep=ep
+                            )
+
+            hidden = output
+            cls_icu,cls_die,cls_tri,cls_poor = cls_model(hidden[:,:1])
+
+            trg_cls=make_trg(sample, train_cls=True)                
+            trg_triage=(trg_cls['trg_triage']).to(DS_model.device)
+            trg_icu7=(trg_cls['trg_icu7']).to(DS_model.device)
+            trg_die7=(trg_cls['trg_die7']).to(DS_model.device)
+            trg_poor7=(trg_cls['trg_icudeath7']).to(DS_model.device)
+            
+            cls_poor2 = torch.max(cls_icu,cls_die)
+
+            loss_icu = criterion(cls_icu,trg_icu7)
+            loss_die = criterion(cls_die,trg_die7)
+            loss_tri = criterion(cls_tri,trg_triage)
+            loss_poor = criterion(cls_poor,trg_poor7)
+            
+            loss = loss_poor + loss_icu + loss_die + loss_dim
+
+            if str(loss.item())=='nan':
+                # debugnan.append([h.cpu(),hm.cpu(),h_emb.cpu(),h_emb_mean.cpu(),h_emb_emb.cpu()])
+                debug_writer(debugnan,tp=2)                       
+            else:
+                loss.sum().backward()
+                model_optimizer_cls.step()
+                model_optimizer.step()
+                model_optimizer_dim.step()
+                    
+                with torch.no_grad():
+                    epoch_loss += loss.item()*bs
+                    epoch_cases += bs
+
+            if iteration % log_interval == 0:
+                print('Ep:{} [{} ({:.0f}%)/ ep_time:{:.0f}min] L:{:.4f} Li:{:.4f} Ld:{:.4f} Lp:{:.4f} Ldim:{:.4f}'.format(
+                        ep, batch_idx * batch_size,
+                        100. * batch_idx / len(dloader),
+                        (time.time()-t0)*len(dloader)/(60*(batch_idx+1)),
+                        loss.item(), loss_icu.item(), loss_die.item(), loss_poor.item(), loss_dim.item(),))
+                
+            if iteration % 200 == 0:
+                try:
+                    save_checkpoint(checkpoint_file=checkpoint_file,
+                                    checkpoint_path='EDisease_CLS.pth',
+                                    model=DS_model,
+                                    parallel=parallel)
+                    save_checkpoint(checkpoint_file=checkpoint_file,
+                                    checkpoint_path='CLS.pth',
+                                    model=cls_model,
+                                    parallel=parallel)
+                    save_checkpoint(checkpoint_file=checkpoint_file,
+                                    checkpoint_path='DIM.pth',
+                                    model=dim_model,
+                                    parallel=parallel)                    
+                except: 
+                    print('** error save checkpoint **')
+                    pass
+            iteration +=1
+        if ep % 1 ==0:
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='EDisease_CLS.pth',
+                            model=DS_model,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='CLS.pth',
+                            model=cls_model,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='DIM.pth',
+                            model=dim_model,
+                            parallel=parallel)
+            
+            val_model = AIED_bert.ewed_Model(config=config,
+                                 tokanizer=BERT_tokenizer,
+                                 device=device)
+            
+            val_DS_model = load_checkpoint(checkpoint_file,'EDisease_CLS.pth',val_model)
+            
+            _, auc = count_tsne_cls(net_F=val_DS_model,
+                               baseBERT=baseBERT,
+                               cls_model=cls_model,
+                               target=dloader_val,
+                               device=device,
+                               use_pi=use_pi,
+                               ki=ep,
+                               draw=False)
+            
+            aucs.append([ep,auc])
+            
+            pd_aucs = pd.DataFrame(aucs)
+            pd_aucs.to_csv('./loss_record/aucs.csv', sep = ',')
+            
+            cls_model.train()
+            if auc > best_auc:
+                # cls_model.train()
+                save_checkpoint(checkpoint_file=checkpoint_file,
+                                checkpoint_path='EDisease_CLS_BEST.pth',
+                                model=DS_model,
+                                parallel=parallel)
+                save_checkpoint(checkpoint_file=checkpoint_file,
+                                checkpoint_path='CLS_BEST.pth',
+                                model=cls_model,
+                                parallel=parallel)                
+                save_checkpoint(checkpoint_file=checkpoint_file,
+                                checkpoint_path='DIM_BEST.pth',
+                                model=dim_model,
+                                parallel=parallel)                
+                best_auc = auc
+
+            print(f'======= epoch:{ep} ======== ** best auc = {best_auc}')
+            
+        loss_pathName = '{:.0f}'.format(time.time())         
+        print('++ Ep Time: {:.1f} Secs ++'.format(time.time()-t0)) 
+        total_loss.append(float(epoch_loss/epoch_cases))
+        pd_total_loss = pd.DataFrame(total_loss)
+        pd_total_loss.to_csv('./loss_record/total_loss_finetune_CLS.csv', sep = ',')
+    print(total_loss)  
+
+
 def make_pickle(DS_model,
                 baseBERT,
                 dloader,
@@ -1271,7 +1479,7 @@ dim_model = AIED_bert.DIM(config=config,
                           device=device,
                           alpha=1,
                           beta=0,
-                          gamma=0.)
+                          gamma=0.1)
 
 expand_model = AIED_bert.ewed_expand_Model(config=config)
 
@@ -2255,7 +2463,7 @@ elif task=='test_nhamcs_cls':
                          collate_fn=AIED_dataloader.collate_fn)
     import AIED_utils    
     
-    ki = 0
+    ki = 'BEST'
     
     try: 
         emb_model = load_checkpoint(checkpoint_file,f'EDisease_CLS_{ki}.pth',emb_model)
@@ -2692,4 +2900,155 @@ elif task=='pickle_nhamcs_cls_val':
                      trainED=True,
                      task=None,
                      checkpoint_file=checkpoint_file,
-                     use_pi=use_pi)     
+                     use_pi=use_pi)  
+    
+elif task=='pickle_nhamcs_cls_dim_val':
+    batch_size = 1024
+    use_pi= False
+    parallel = False
+    try: 
+        emb_model = load_checkpoint(checkpoint_file,'EDisease_CLS.pth',pickle_Model)
+        print(' ** Complete Load CLS EDisease Model ** ')
+    except:
+        print('*** No Pretrain_EDisease_CLS_Model ***')
+
+        try: 
+            emb_model = load_checkpoint(checkpoint_file,'EDisease.pth',pickle_Model)
+            print(' ** Complete Load EDisease Model ** ')
+        except:
+            print('*** No Pretrain_EDisease_Model ***')
+            emb_model = pickle_Model
+            success_load = 1
+
+    try:     
+        dim_model = load_checkpoint(checkpoint_file,'DIM.pth',dim_model)
+    except:
+        print('*** No Pretrain_DIM_Model ***')
+        pass
+
+    try:     
+        cls_model = load_checkpoint(checkpoint_file,'CLS.pth',cls_model)
+    except:
+        print('*** No Pretrain_cls_Model ***')
+        pass
+
+# ====
+    all_datas = AIED_dataloader.load_datas()
+    datas_train = all_datas['datas_train']
+    dm_normalization_np = all_datas['dm_normalization_np']   
+    datas_test = all_datas['datas_val']
+    datas_val = all_datas['datas_test']  
+    datas_all = all_datas['datas']
+    
+    # datas_val = datas_val[datas_val['AGE']>=18]
+    
+    data15_triage_val_sample = datas_val.reset_index()
+
+    EDEW_DS_val = AIED_dataloader.EDEW_Dataset(ds= data15_triage_val_sample,          
+                                           tokanizer= BERT_tokenizer,
+                                           normalization = dm_normalization_np,)  
+    
+    EDEW_DL_val = DataLoader(dataset = EDEW_DS_val,
+                         shuffle = False,
+                         num_workers=4,
+                         batch_size=128,
+                         collate_fn=AIED_dataloader.collate_fn)
+# ====
+
+    print('** Start load pickle **')
+    
+    pklfile = 'nhamcs.pickle'
+    with open(pklfile,'rb') as f:
+        data15_triage_train = pickle.load(f)  
+    print('** complete load pickle **')
+    
+    if not use_pi:
+        data15_triage_train['piemb']=0
+        
+    structural = ['AGE',
+                'SEX',
+                'GCS',
+                'BPSYS',
+                'BPDIAS',
+                'PULSE',
+                'POPCT',
+                'RESPR',
+                'TEMPF',
+                'HEIGHT', 
+                'WEIGHT',
+                'PAINSCALE',
+                'BE',
+                'BV',
+                'BM'
+                ]
+        
+    dm = data15_triage_train[structural]
+    dm_mean = dm.mean(axis=0, skipna=True)
+    dm_std = dm.std(axis=0, skipna=True)
+    dm_normalization = pd.concat([dm_mean,dm_std],axis=1)
+    print('trainset',dm_normalization)
+    dm_normalization_np = np.array(dm_normalization).T  
+    
+    
+    # data15_triage_train = data15_triage_train[data15_triage_train['AGE']>=18]
+    
+    data15_triage_train = data15_triage_train.reset_index()
+                             
+    rn0 = data15_triage_train['DIEDED']>7
+    rp0 = data15_triage_train['DIEDED']<8
+    
+    rn1 = data15_triage_train['ICU']>7
+    rp1 = data15_triage_train['ICU']<8
+    
+    rn2 = rn1 & rn0
+    rp2 = rp1 | rp0    
+
+    # g00 = pd.Series(data15_triage_train[rn0 & rn1].index)
+    # g01 = pd.Series(data15_triage_train[rn0 & rp1].index)
+    # g10 = pd.Series(data15_triage_train[rp0 & rn1].index)
+    # g11 = pd.Series(data15_triage_train[rp0 & rp1].index)
+
+    # ltemp = [g00] + [g01]*int(len(g00)/len(g01)) +[g10]*int(len(g00)/len(g10)) +[g11]*int(len(g00)/len(g11))
+    # print(len(g00),len(g01),len(g10),len(g11))
+    # print(' *** balance the dataset p/n *** ')
+    # dtemp = pd.concat(ltemp)  
+    # print(' *** balance complete *** ')
+
+    g00 = pd.Series(data15_triage_train[rn0 & rn1].index)
+    g01 = pd.Series(data15_triage_train[rn0 & rp1].index)
+    g10 = pd.Series(data15_triage_train[rp0 & rn1].index)
+    g11 = pd.Series(data15_triage_train[rp0 & rp1].index)
+
+    ltemp = [g00] + ([g01]*2 + [g10]*int(2*len(g01)/len(g10)) +[g11]*300)*16  #16for all 14for adult
+    print(len(g00),len(g01),len(g10),len(g11))
+    print(' *** balance the dataset p/n *** ')
+    dtemp = pd.concat(ltemp)  
+    print(' *** balance complete *** ')
+
+    EDEW_DS = AIED_dataloader.pickle_Dataset(ds= data15_triage_train,          
+                                           tokanizer= BERT_tokenizer,
+                                           normalization = dm_normalization_np,
+                                           dsidx=dtemp,)  
+
+    EDEW_DL = DataLoader(dataset = EDEW_DS,
+                         shuffle = True,
+                         num_workers=10,
+                         batch_size=batch_size,
+                         )
+
+    train_NHAMCS_cls_dim_val(DS_model=emb_model,
+                     cls_model=cls_model,
+                     dim_model=dim_model,
+                     baseBERT=baseBERT,
+                     dloader=EDEW_DL,
+                     dloader_val=EDEW_DL_val,
+                     noise_scale=0.05,
+                     mask_ratio=0.33,
+                     lr=2e-5,
+                     epoch=100001,
+                     log_interval=15,
+                     parallel=parallel,
+                     trainED=True,
+                     task=None,
+                     checkpoint_file=checkpoint_file,
+                     use_pi=use_pi) 
